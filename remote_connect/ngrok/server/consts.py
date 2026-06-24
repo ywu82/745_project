8af3ef0e11f4f77f98d1d4bd16919a165262f7e2
ywu82 <ywu82@nyit.edu>
2026-06-24 01:52:34 -0700

    Add environment constants for ngrok configuration

---
+++ remote_connect/ngrok/server/consts.py
@@ -0,0 +1,10 @@
+ENV = {
+    "MONGO_URI": "<your own mongodb key>",
+    "MONGO_DB": "my_data_center",
+    "MONGO_COLLECTION": "ngrok_status",
+
+    "NGROK_BIN": "ngrok",
+    "LOCAL_SSH_PORT": "22",
+    "CHECK_INTERVAL": "10",
+    "SERVICE_ID": "wsl2-ssh",
+}
