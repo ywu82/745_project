bd9dd7be5c295b0dd8a1dce6cff970bf874cff1a
ywu82 <ywu82@nyit.edu>
2026-06-24 01:51:42 -0700

    Add consts.py with configuration constants

---
+++ remote_connect/ngrok/client/consts.py
@@ -0,0 +1,12 @@
+ENV = {
+    "MONGO_URI": "<YOUR Mongodb Atlas Token>",
+    "MONGO_DB": "my_data_center",
+    "MONGO_COLLECTION": "ngrok_status",
+
+    "SERVICE_ID": "wsl2-ssh",
+    "MAX_STATUS_AGE_SECONDS": 60,
+    # Default username, can be empty.
+    # If empty, the user will be prompted to enter it at runtime.
+    "DEFAULT_SSH_USER": "",
+    
+}
